f330dd35998b99346f5d8f40226c843e01d6df40
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-21 14:40:14 +0200

    img in htmlpagina

--- www/script.js
+++ www/script.js
@@ -20,16 +20,6 @@ function onDeviceReady() {
 	alert('device ready');
 	var objCanvas = document.getElementById("canvas");
 	window.plugin.CanvasCamera.initialize(objCanvas);
-
-
-	//document.getElementById("takePicture").addEventListener("click", takePicture, false);
-	document.getElementById("takePicturePreview").addEventListener("click", onTakePicture, false);
-
-}
-
-function onTakePicture() {
-	CanvasCamera.takePicture(onTakeSuccess);
-	alert('onTakePicture');
 	var opt = {
 		quality: 75,
 		destinationType: CanvasCamera.DestinationType.DATA_URL,
@@ -41,49 +31,44 @@ function onTakePicture() {
 	};
 	CanvasCamera.start(opt);
 	alert('canvas ready');
+
+	//document.getElementById("takePicture").addEventListener("click", takePicture, false);
+	document.getElementById("takePicturePreview").addEventListener("click", onTakePicture, false);
+
+}
+
+function onTakePicture() {
+	CanvasCamera.takePicture(onTakeSuccess);
+	alert('onTakePicture');
 }
 
 function onTakeSuccess(data) {
-
-		var objCanvas = document.getElementById("canvas");
-		var context = objCanvas.getContext("2d");
-
-		objCanvas.width = window.innerWidth;
-		objCanvas.height = window.innerHeight;
-
-		var x = 0;
-		var y = 0;
-		var width = window.innerWidth;
-		var height = window.innerHeight;
-		var imageObj = new Image();
-		objCanvas.src = "data:image/jpeg;base64," + data;
-		imageObj.onload = function () {
-			context.drawImage(imageObj, x, y, width, height);
-		};
-			 // options.encodingType == CanvasCamera.EncodingType.JPEG
-			// image.src = "data:image/png;base64," + data; // options.encodingType == CanvasCamera.EncodingType.PNG
-		}
+	
+	var image = document.getElementById("myImage");
+	image.src = "data:image/jpeg;base64," + data; // options.encodingType == CanvasCamera.EncodingType.JPEG
+	// image.src = "data:image/png;base64," + data; // options.encodingType == CanvasCamera.EncodingType.PNG
+}
 
 
 
-		/*function takePicture(e) {
-			navigator.camera.getPicture(onSuccess, onFail, {
-				quality: 50,
-				destinationType: navigator.camera.DestinationType.DATA_URL,
-				allowEdit: true,
-				targetWidth: 50,
-				targetHeight: 50
-			});
-		}
+/*function takePicture(e) {
+	navigator.camera.getPicture(onSuccess, onFail, {
+		quality: 50,
+		destinationType: navigator.camera.DestinationType.DATA_URL,
+		allowEdit: true,
+		targetWidth: 50,
+		targetHeight: 50
+	});
+}
 
-		function onSuccess(imageData) {
-			var image = document.getElementById('myImage');
-			image.src = "data:image/jpeg;base64," + imageData;
-		}
+function onSuccess(imageData) {
+	var image = document.getElementById('myImage');
+	image.src = "data:image/jpeg;base64," + imageData;
+}
 
-		function onFail(message) {
-			setTimeout(function () {
-				//alert('Failed because: ' + message);
-			}, 0);
+function onFail(message) {
+	setTimeout(function () {
+		//alert('Failed because: ' + message);
+	}, 0);
 
-		}*/
+}*/

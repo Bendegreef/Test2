faa9adb366f6c8a5ce09aaf2e60136d2886e858b
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-21 14:00:26 +0200

    test3

--- www/script.js
+++ www/script.js
@@ -30,7 +30,7 @@ function onDeviceReady() {
 function onTakePicture() {
 	CanvasCamera.takePicture(onTakeSuccess);
 	alert('onTakePicture');
-		var opt = {
+	var opt = {
 		quality: 75,
 		destinationType: CanvasCamera.DestinationType.DATA_URL,
 		encodingType: CanvasCamera.EncodingType.JPEG,
@@ -44,32 +44,46 @@ function onTakePicture() {
 }
 
 function onTakeSuccess(data) {
-	
-	var objCanvas = document.getElementById("canvas");
-	objCanvas.src = "data:image/jpeg;base64," + data; // options.encodingType == CanvasCamera.EncodingType.JPEG
-	// image.src = "data:image/png;base64," + data; // options.encodingType == CanvasCamera.EncodingType.PNG
-}
+
+		var objCanvas = document.getElementById("canvas");
+		var context = objCanvas.getContext('2d');
+
+		objCanvas.width = window.innerWidth;
+		objCanvas.height = window.innerHeight;
+
+		var x = 0;
+		var y = 0;
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		var imageObj = new Image();
+
+		imageObj.onload = function () {
+			context.drawImage(imageObj, x, y, width, height);
+		};
+			objCanvas.src = "data:image/jpeg;base64," + data; // options.encodingType == CanvasCamera.EncodingType.JPEG
+			// image.src = "data:image/png;base64," + data; // options.encodingType == CanvasCamera.EncodingType.PNG
+		}
 
 
 
-/*function takePicture(e) {
-	navigator.camera.getPicture(onSuccess, onFail, {
-		quality: 50,
-		destinationType: navigator.camera.DestinationType.DATA_URL,
-		allowEdit: true,
-		targetWidth: 50,
-		targetHeight: 50
-	});
-}
+		/*function takePicture(e) {
+			navigator.camera.getPicture(onSuccess, onFail, {
+				quality: 50,
+				destinationType: navigator.camera.DestinationType.DATA_URL,
+				allowEdit: true,
+				targetWidth: 50,
+				targetHeight: 50
+			});
+		}
 
-function onSuccess(imageData) {
-	var image = document.getElementById('myImage');
-	image.src = "data:image/jpeg;base64," + imageData;
-}
+		function onSuccess(imageData) {
+			var image = document.getElementById('myImage');
+			image.src = "data:image/jpeg;base64," + imageData;
+		}
 
-function onFail(message) {
-	setTimeout(function () {
-		//alert('Failed because: ' + message);
-	}, 0);
+		function onFail(message) {
+			setTimeout(function () {
+				//alert('Failed because: ' + message);
+			}, 0);
 
-}*/
+		}*/

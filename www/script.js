f60f65ab2683cd8d71fd3d9ca0fc3d0ff16c1912
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-22 10:33:17 +0200

    gs

--- www/script.js
+++ www/script.js
@@ -68,8 +68,6 @@ function onSuccess(imageData) {
 	var context = canvas.getContext("2d");
 	var imageObj = document.getElementById("myImage");
 	imageObj.src = "data:image/jpeg;base64," + imageData;
-
-
 	var width = document.getElementById("myImage").width;
 	context.drawImage(imageObj, 0, 0, width, width, 0, 0, 360, 360);
 	//imageObj.src = "data:image/jpeg;base64," + imageData;
